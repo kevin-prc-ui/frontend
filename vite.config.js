05fdc08e1d1e5be9bb2b3f30da0d44f746479a2d
kevin-prc-ui <nemecis_758@hotmail.com>
2025-06-16 12:16:00 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-
-  ],
+  plugins: [react(), tailwindcss()],
   define: {
-    global: 'window', // Soluciona el problema de 'global is not defined'
+    global: "window", // Soluciona el problema de 'global is not defined'
   },
-  server:{
-    allowedHosts: ['all','localhost'],
-    port:3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:8080",
-        changeOrigin:true,
-      }
-    }
-  }
-})
+  server: {
+    allowedHosts: ["all", "localhost"],
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
+});
